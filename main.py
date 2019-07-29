8b43cb199b7af1ab0d54d561b50d82eaf244e46a
Pranav Natekar <pranavnat24@gmail.com>
2019-07-30 00:29:39 +0530

    Added resizing and QTabWidget

--- main.py
+++ main.py
@@ -16,13 +16,24 @@ class MainApp(QtWidgets.QMainWindow, gui.Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainApp, self).__init__(parent)
         self.setupUi(self)
+
+        self.extention = '.jpg'
+
+        self.dirName = 'Augmented'
         self.setWindowTitle("Image Augmenter")
+        if not os.path.exists(self.dirName):
+            os.mkdir(self.dirName)
+            print("Directory " , self.dirName ,  " Created ")
+        else:
+            print("Directory " , self.dirName ,  " already exists")
 
         sys.stdout = Stream(newText=self.onUpdateText)
 
+        # Function Calls
         self.onbrowse()
         self.onshow()
         self.onexit()
+        self.onaugmentclicked()
 
     def onUpdateText(self, text):
         cursor = self.cmdop.textCursor()
@@ -51,9 +62,32 @@ class MainApp(QtWidgets.QMainWindow, gui.Ui_MainWindow):
         self.image = cv2.imread(self.fileName)
         cv2.imshow("Loaded Image", self.image)
         print("Done Displaying")
-
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+
+    def resizeimage(self, image, w, h):
+        self.resized_image = cv2.resize(self.image, (w, h))
+        cv2.imwrite(self.dirName+"/Resized-"+str(w)+"*"+str(h)+self.extention, self.resized_image)
+        print("Resizing done with "+ str(w) +" * "+ str(h) +" resolution")
+
+    def onaugmentclicked(self):
+        self.augment.clicked.connect(self.onaugment)
+
+    def onaugment(self):
+        # Customize your resolutions
+        self.resizeimage(self.image, 450, 400)
+        self.resizeimage(self.image, 200, 200)
+        self.resizeimage(self.image, 500, 350)
+        self.resizeimage(self.image, 600, 500)
+        self.resizeimage(self.image, 250, 350)
+        self.resizeimage(self.image, 550, 400)
+        self.resizeimage(self.image, 150, 100)
+        self.resizeimage(self.image, 150, 120)
+        self.resizeimage(self.image, 700, 600)
+        self.resizeimage(self.image, 550, 500)
+        self.resizeimage(self.image, 200, 350)
+        self.resizeimage(self.image, 500, 200)
+
 
     def onexit(self):
         self.quit.clicked.connect(sys.exit)
