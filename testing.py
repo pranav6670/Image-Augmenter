79089d0b9bec216787cece258ba490032a2294c6
Pranav Natekar <pranavnat24@gmail.com>
2019-07-29 21:49:35 +0530

    Tidied up the GUI

--- testing.py
+++ testing.py
@@ -3,24 +3,39 @@ from PyQt5 import QtGui, QtCore, QtWidgets
 import gui
 import os
 import numpy as np
+import sys
 
+class Stream(QtCore.QObject):
+    newText = QtCore.pyqtSignal(str)
+
+    def write(self, text):
+        self.newText.emit(str(text))
 
 
 class MainApp(QtWidgets.QMainWindow, gui.Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainApp, self).__init__(parent)
         self.setupUi(self)
+        self.setWindowTitle("Image Augmenter")
+
+        sys.stdout = Stream(newText=self.onUpdateText)
+
         self.onbrowse()
         self.onshow()
+        self.onexit()
 
-
+    def onUpdateText(self, text):
+        cursor = self.cmdop.textCursor()
+        cursor.movePosition(QtGui.QTextCursor.End)
+        cursor.insertText(text)
+        self.cmdop.setTextCursor(cursor)
+        self.cmdop.ensureCursorVisible()
 
     def onbrowse(self):
         self.browse.clicked.connect(self.openFileNameDialog)
 
     def onshow(self):
         self.showim.clicked.connect(self.showimage)
-
 
     def openFileNameDialog(self):
         options = QtWidgets.QFileDialog.Options()
@@ -31,19 +46,22 @@ class MainApp(QtWidgets.QMainWindow, gui.Ui_MainWindow):
         if self.fileName:
             print(self.fileName)
 
-
     def showimage(self):
+        print("Displaying...")
         self.image = cv2.imread(self.fileName)
         cv2.imshow("Loaded Image", self.image)
+        print("Done Displaying")
+
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
+    def onexit(self):
+        self.quit.clicked.connect(sys.exit)
 
-
-
+    def __del__(self):
+        sys.stdout = sys.__stdout__
 
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
     w = MainApp()
     w.show()
